2fe8a9bac576fbd0cb544e65a5f10a8678ad21c3
Manish Raj <42971673+manishrajmishra@users.noreply.github.com>
2020-12-26 02:31:18 +0530

    added the config file of firebase

--- src/firebase.js
+++ src/firebase.js
@@ -13,16 +13,16 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyCq8s1NyRd2M9Z83uX8BsLFIP-elQQe3Hc",
-  authDomain: "todo-app-1c268.firebaseapp.com",
-  databaseURL: "https://todo-app-1c268.firebaseio.com",
-  projectId: "todo-app-1c268",
-  storageBucket: "todo-app-1c268.appspot.com",
-  messagingSenderId: "442023828592",
-  appId: "1:442023828592:web:2613cb826fc364a3de28b5",
-  measurementId: "G-2BQ8TP02YX"
+  apiKey: "AIzaSyDLEmAGbFJE8eTmh2LFLO9vupMqaD9EcsY",
+  authDomain: "todo-appp-react.firebaseapp.com",
+  databaseURL: "https://todo-appp-react.firebaseio.com",
+  projectId: "todo-appp-react",
+  storageBucket: "todo-appp-react.appspot.com",
+  messagingSenderId: "1054797573335",
+  appId: "1:1054797573335:web:a244aacfb5f9a886b0d492",
+  measurementId: "G-BXTE5JK6RC"
 });
 
 const db = firebaseApp.firestore();
 
-export { db };
+export default db;
