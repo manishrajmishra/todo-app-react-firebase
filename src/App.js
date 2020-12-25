39eab84ba8096b0258769717a663acc3e64a40b0
Manish Raj <42971673+manishrajmishra@users.noreply.github.com>
2020-12-25 20:21:41 +0530

    setting todos array

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
-
+const userName = prompt("What is your Name?");
 export default function App() {
+  const [todos, setTodos] = useState([
+    "Take dogs for a walk",
+    "Bring milk from market"
+  ]);
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Hello, {userName}</h1>
+      <input />
+      <button>Add Todo</button>
+      <ul>
+        {todos.map((todo) => (
+          <li>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
 }
