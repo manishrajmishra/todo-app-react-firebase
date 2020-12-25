be9eebd3a5ccfbd6ee0710b45b5aab9a4f4b93d1
Manish Raj <42971673+manishrajmishra@users.noreply.github.com>
2020-12-25 20:34:31 +0530

    feat: adding the todos in the previous todos

--- src/App.js
+++ src/App.js
@@ -4,13 +4,20 @@ const userName = prompt("What is your Name?");
 export default function App() {
   const [todos, setTodos] = useState([
     "Take dogs for a walk",
-    "Bring milk from market"
+    "Bring milk from market",
+    "Buy some chocolates"
   ]);
+  const [input, setInput] = useState("");
+
+  const addTodo = (event) => {
+    setTodos([...todos, input]);
+  };
+
   return (
     <div className="App">
       <h1>Hello, {userName}</h1>
-      <input />
-      <button>Add Todo</button>
+      <input value={input} onChange={(event) => setInput(event.target.value)} />
+      <button onClick={addTodo}>Add Todo</button>
       <ul>
         {todos.map((todo) => (
           <li>{todo}</li>
